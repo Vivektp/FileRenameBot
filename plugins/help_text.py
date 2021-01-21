eadb6635e5cda0889fa0ba9f11f40e30d45c2913
Vivek <75740863+Vivektp@users.noreply.github.com>
2021-01-21 08:49:05 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -70,12 +70,12 @@ async def start(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Channel', url='https://t.me/VKPROJECTS'),
+                    InlineKeyboardButton('Feedback', url='https://t.me/VKPBOTS')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Other Bots', url='https://t.me/VKPROJECTS'),
+                    InlineKeyboardButton('Source', url='https://t.me//VKPROJECTS)
                 ]
             ]
         ),
